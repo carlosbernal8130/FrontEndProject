65d40b57604ed17d4aed7fb69cef0bbf0f371e06
carlosbernal8130 <canbere@gmail.com>
2024-09-22 18:49:38 -0500

    Entrega2

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function consulta_pin() {
+    let url ="https://www.enlace-apb.com/interssi/consultarPinFueraPagina.jsp"
+    window.open(url,'_blank');
+};
+
+function consulta_certificado() {
+    let url ="https://www.enlace-apb.com/interssi/descargarCertificacionPago.jsp"
+    window.open(url,'_blank');
+};
